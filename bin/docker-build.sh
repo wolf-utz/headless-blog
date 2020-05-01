eb67f499b51fbbe6643f7de0c8e91670323e948d
Wolf Utz <wpu@hotmail.de>
2020-05-01 22:00:45 +0200

    Add SQL schema and install script

--- bin/docker-build.sh
+++ bin/docker-build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 echo "[INFO] Running a composer install"
-docker exec app /bin/bash -c "composer update"
+docker exec app /bin/bash -c "composer install"
 docker exec app /bin/bash -c "chown -R www-data:www-data ."
---
+++ res/schema.sql
@@ -0,0 +1,33 @@
+CREATE TABLE IF NOT EXISTS post (
+    id int(11) NOT NULL AUTO_INCREMENT,
+    title varchar(255) DEFAULT '' NOT NULL,
+    active tinyint(2) DEFAULT 0 NOT NULL,
+    teaser tinytext,
+    body text,
+    created_at datetime DEFAULT NOW() NOT NULL,
+    updated_at datetime DEFAULT NOW() NOT NULL,
+    PRIMARY KEY (id)
+);
+CREATE TABLE IF NOT EXISTS tag (
+    id int(11) NOT NULL AUTO_INCREMENT,
+    title varchar(255) DEFAULT '' NOT NULL,
+    PRIMARY KEY (id)
+);
+CREATE TABLE IF NOT EXISTS category (
+    id int(11) NOT NULL AUTO_INCREMENT,
+    title varchar(255) DEFAULT '' NOT NULL,
+    parent int DEFAULT 0 NOT NULL,
+    PRIMARY KEY (id)
+);
+CREATE TABLE IF NOT EXISTS post_tag (
+    post_id int DEFAULT 0 NOT NULL,
+    tag_id int DEFAULT 0 NOT NULL,
+    FOREIGN KEY (post_id) REFERENCES post (id),
+    FOREIGN KEY (tag_id) REFERENCES tag (id)
+);
+CREATE TABLE IF NOT EXISTS post_category (
+    post_id int DEFAULT 0 NOT NULL,
+    category_id int DEFAULT 0 NOT NULL,
+    FOREIGN KEY (post_id) REFERENCES post (id),
+    FOREIGN KEY (category_id) REFERENCES category (id)
+);
