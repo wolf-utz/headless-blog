9eb2d59c24dcc1e7d766e0637caf9d606ba3b434
Wolf Utz <w.utz@psv-neo.de>
2021-02-15 00:08:51 +0100

    Add deployment test #10

--- .ci/scripts/deploy.sh
+++ .ci/scripts/deploy.sh
@@ -1,20 +1,21 @@
 #!/usr/bin/env bash
 
-# Login to server via ssh.
+echo "Login to server via ssh."
 which ssh-agent || ( apk --no-cache add openssh-client )
 eval "$(ssh-agent -s)"
 
 SSH_PRIVATE_KEY=$(echo "$SSH_PRIVATE_KEY" | base64 -d)
 echo "$SSH_PRIVATE_KEY" | tr -d '\r' | ssh-add - > /dev/null
 
-# Add known_hosts.
+echo "Add known_hosts."
 SSH_HOST="lupus-code.it"
-mkdir -p ~/.ssh
-chmod 700 ~/.ssh
-ssh-keyscan -H "${SSH_HOST}" >> ~/.ssh/known_hosts
-chmod 644 ~/.ssh/known_hosts
+mkdir -p "$HOME"/.ssh
+chmod 700 "$HOME"/.ssh
+ssh-keyscan -H "${SSH_HOST}" >> "$HOME"/.ssh/known_hosts
+chmod 644 "$HOME"/.ssh/known_hosts
 
 # Install composer and deployer.
+echo "Install composer and deployer and deploy."
 curl -sS https:/getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer \
 composer global require -q --dev deployer/deployer:~6.4 deployphp/recipes:~6.2
 "$HOME"/.composer/vendor/bin/dep -f./.ci/deploy.php deploy production
