d7b0a1851780289ccfdb4a3fb820742e35feebff
Wolf Utz <w.utz@psv-neo.de>
2021-02-15 23:24:08 +0100

    Add deployment test #23

--- .ci/scripts/deploy.sh
+++ .ci/scripts/deploy.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
 echo "Install deployer and deploy to destination:"
-composer global require -q --dev deployer/deployer:~6.4 deployphp/recipes:~6.2
-~/.composer/vendor/bin/dep -f./.ci/deploy.php deploy production
+vendor/bin/dep -f./.ci/deploy.php deploy production
